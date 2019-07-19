0d5fc4488739521b09531086e5b857ccaf2a4835
Nick Schweitzer <nick@thecodingmonkey.net>
2019-07-19 10:25:08 -0500

    Update to VS2019

--- TextSerializer/Properties/AssemblyInfo.cs
+++ TextSerializer/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("The Coding Monkey")]
 [assembly: AssemblyProduct("Text Serializer")]
-[assembly: AssemblyCopyright("Copyright 2017")]
+[assembly: AssemblyCopyright("Copyright 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
