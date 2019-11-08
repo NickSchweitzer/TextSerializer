78b054d800493e6532389e6db70bebeaef10a8cf
Nick Schweitzer <nick@thecodingmonkey.net>
2019-11-08 11:40:56 -0600

    Additional unit tests around Attributes

---
+++ Tests/AttributeTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TheCodingMonkey.Serialization.Formatters;
+
+namespace TheCodingMonkey.Serialization.Tests
+{
+    [TestClass, TestCategory("Attribute")]
+    public class AttributeTests
+    {
+        [TestMethod]
+        public void CsvAttributeTest()
+        {
+            TextFieldAttribute attribute = new TextFieldAttribute(1)
+            {
+                Size = 50,
+                Name = "TestField",
+                Optional = true,
+                FormatterType = typeof(BooleanIntFormatter)
+            };
+
+            Assert.AreEqual(1, attribute.Position);
+            Assert.AreEqual(50, attribute.Size);
+            Assert.AreEqual("TestField", attribute.Name);
+            Assert.AreEqual(true, attribute.Optional);
+            Assert.AreEqual(typeof(BooleanIntFormatter), attribute.FormatterType);
+        }
+
+        [TestMethod]
+        public void FixedWidthAttributeTest()
+        {
+            FixedWidthFieldAttribute attribute = new FixedWidthFieldAttribute(1, 50)
+            {
+                Padding = '0'
+            };
+
+            Assert.AreEqual(1, attribute.Position);
+            Assert.AreEqual(50, attribute.Size);
+            Assert.AreEqual('0', attribute.Padding);
+        }
+    }
+}
